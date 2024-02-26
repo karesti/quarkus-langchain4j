54773e59cb935d5861c75b3fd83ec14e5208248a
Katia Aresti <karesti@redhat.com>
2024-02-26 15:55:45 +0100

    infinispan chatbot

---
+++ samples/chatbot-infinispan/src/main/java/io/quarkiverse/langchain4j/sample/chatbot/RetrieverExample.java
@@ -0,0 +1,28 @@
+package io.quarkiverse.langchain4j.sample.chatbot;
+
+import java.util.List;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.retriever.EmbeddingStoreRetriever;
+import dev.langchain4j.retriever.Retriever;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+
+@ApplicationScoped
+public class RetrieverExample implements Retriever<TextSegment> {
+
+    private final EmbeddingStoreRetriever retriever;
+
+    @Inject
+    RetrieverExample(EmbeddingStore store, EmbeddingModel model) {
+        retriever = EmbeddingStoreRetriever.from(store, model, 20);
+    }
+
+    @Override
+    public List<TextSegment> findRelevant(String s) {
+        return retriever.findRelevant(s);
+    }
+}
